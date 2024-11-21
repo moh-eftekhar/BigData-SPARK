eb7a345d8bab08577eed4fe6602ec9696c8f4886
moeftekhar <mohammad.eftekharipour@studenti.polito.it>
2024-11-21 11:24:22 +0100

    update

--- LABS/LAB6/test.py
+++ LABS/LAB6/test.py
@@ -1,9 +1,4 @@
 x = [1,2,3,4]
 for i in range(len(x)):
     for j in range(i+1, len(x)):
-        if (x[i], x[j]) != (x[0], x[1]):
             print(x[i], x[j])
-
-
-
-.reduceByKey(lambda x, y: x + y)
