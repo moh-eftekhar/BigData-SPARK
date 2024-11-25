4bb906d4cb50ef6a88d0110707f2a5a368dd03a1
moeftekhar <mohammad.eftekharipour@studenti.polito.it>
2024-11-25 17:59:16 +0100

    udate

--- LABS/LAB6/test.py
+++ LABS/LAB6/test.py
@@ -1,4 +1,25 @@
-x = [1,2,3,4]
-for i in range(len(x)):
-    for j in range(i+1, len(x)):
-            print(x[i], x[j])
+# def new_func():
+#     x = [1,2,3,4]
+#     for i in range(len(x)):
+#         for j in range(i+1, len(x)):
+#                 print(x[i], x[j])
+data = [
+    (('B1', 'B3'), 1),
+    (('B1', 'B5'), 1),
+    (('B3', 'B5'), 1),
+    (('B1', 'B3'), 1),
+    (('B1', 'B4'), 1),
+    (('B1', 'B5'), 1),
+    (('B3', 'B4'), 1),
+    (('B3', 'B5'), 1),
+    (('B4', 'B5'), 1),
+    (('B5', 'B1'), 1),
+    (('B5', 'B3'), 1),
+    (('B1', 'B3'), 1),
+]
+sorted_tuple = sorted(data, key=lambda x: x[0])
+print("------->>>>>>>",sorted_tuple)  # Output: ['B1', 'B3']
+
+# Convert back to a tuple
+normalized_tuple = tuple(sorted_tuple)
+print("---------//////>>>>",normalized_tuple)  # Output: ('B1', 'B3')
